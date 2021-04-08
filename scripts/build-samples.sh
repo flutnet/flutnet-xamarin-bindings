61e515f2e43fac5e3d39978453dfba850c656a7c
micneg <michele.negri@novagemsolutions.com>
2021-04-08 09:08:56 -0700

    - piccole migliorie

--- scripts/build-samples.sh
+++ scripts/build-samples.sh
@@ -6,12 +6,12 @@ SAMPLES_DIR=$SCRIPT_DIR/../samples
 VSTOOL_PATH="/Applications/Visual Studio.app/Contents/MacOS/vstool"
 SOLUTION_PATH=$SAMPLES_DIR/Flutnet.Interop.Samples.sln
 
-# Build Flutter module
+# Build Flutter module (Debug configuration is enough)
 cd "$SAMPLES_DIR/flutter_module"
 flutter pub get
-flutter build aar --no-profile
-flutter build ios-framework --no-profile
+flutter build aar --no-profile --no-release
+flutter build ios-framework --no-profile --no-release
 
-# Clean and build sample projects
+# Clean and build sample projects (Debug configuration is enough)
 "$VSTOOL_PATH" build --configuration:Debug --target:Clean "$SOLUTION_PATH"
 "$VSTOOL_PATH" build --configuration:Debug "$SOLUTION_PATH"
