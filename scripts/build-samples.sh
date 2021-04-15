9bd255b6be614262a61f78e72b17f2431a9a2e1b
micneg <michele.negri@novagemsolutions.com>
2021-04-15 11:28:11 +0200

    Added NuGet packages restore to build scripts

--- scripts/build-samples.sh
+++ scripts/build-samples.sh
@@ -4,6 +4,7 @@
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SAMPLES_DIR=$SCRIPT_DIR/../samples
 VSTOOL_PATH="/Applications/Visual Studio.app/Contents/MacOS/vstool"
+MSBUILD_PATH=/Library/Frameworks/Mono.framework/Versions/Current/bin/msbuild
 SOLUTION_PATH=$SAMPLES_DIR/Flutnet.Interop.Samples.sln
 
 # Build Flutter module (Debug configuration is enough)
@@ -11,6 +12,12 @@ cd "$SAMPLES_DIR/flutter_module"
 flutter pub get
 flutter build aar --no-profile --no-release
 flutter build ios-framework --no-profile --no-release
+
+# Restore Nuget Packages
+# The following command does NOT work for Xamarin projects
+# https://xamarin.github.io/bugzilla-archives/58/58254/bug.html
+# dotnet restore "$SOLUTION_PATH"
+"$MSBUILD_PATH" /t:restore "$SOLUTION_PATH"
 
 # Clean and build sample projects (Debug configuration is enough)
 "$VSTOOL_PATH" build --configuration:Debug --target:Clean "$SOLUTION_PATH"
--- scripts/build-src.sh
+++ scripts/build-src.sh
@@ -4,7 +4,14 @@
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 SRC_DIR=$SCRIPT_DIR/../src
 VSTOOL_PATH="/Applications/Visual Studio.app/Contents/MacOS/vstool"
+MSBUILD_PATH=/Library/Frameworks/Mono.framework/Versions/Current/bin/msbuild
 SOLUTION_PATH=$SCRIPT_DIR/../Flutnet.Interop.sln
+
+# Restore Nuget Packages
+# The following command does NOT work for Xamarin projects
+# https://xamarin.github.io/bugzilla-archives/58/58254/bug.html
+# dotnet restore "$SOLUTION_PATH"
+"$MSBUILD_PATH" /t:restore "$SOLUTION_PATH"
 
 # Clean and build binding libraries for all the available configurations
 "$VSTOOL_PATH" build --configuration:Debug --target:Clean "$SOLUTION_PATH"
